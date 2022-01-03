ec596566b09e22f7ba9cbea654ec0bc467033442
docapc <armel.pitelet@gmail.com>
2022-01-03 16:26:40 +0100

    Premières Notes sur Wpf

---
+++ WPF/Exercices/DemoWpf/DemoWpf/Texte.cs
@@ -0,0 +1,16 @@
+﻿namespace DemoWpf
+{
+    public class Texte
+    {
+        private static int _sid = 0;
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Texte(string texte)
+        {
+            Id = ++_sid;
+            Name = texte;
+        }
+    }
+}
---
+++ WPF/Exercices/DemoWpf/DemoWpf/TexteManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoWpf
+{
+    public class TexteManager
+    {
+        public List<Texte> Textes { get; set; } = new List<Texte>() { new Texte("azer"), new Texte("ty") };
+
+        public TexteManager()
+        {
+
+        }
+
+    }
+}
