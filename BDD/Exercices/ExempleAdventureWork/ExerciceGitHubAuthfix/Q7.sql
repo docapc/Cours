285f57cfde4627657c9630ec7e1a145d3285d4eb
docapc <armel.pitelet@gmail.com>
2022-01-10 16:03:16 +0100

    modif .gitignore pour garder les .bak

--- BDD/Exercices/ExempleAdventureWork/ExerciceGitHubAuthfix/Q7.sql
+++ BDD/Exercices/ExempleAdventureWork/ExerciceGitHubAuthfix/Q7.sql
@@ -1,1 +1,8 @@
 /*Retrouvez le nom du produit et le nom de la compagnie pour les clients ayant commandés le produit 'Racing Socks' */
+
+SELECT * 
+FROM SalesLT.Customer
+
+SELECT * 
+FROM SalesLT.Product
+ORDER BY SalesLT.Product.Name
