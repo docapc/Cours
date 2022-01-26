4021efbcd8e036acbbe2df1ca5600399e9a8f727
docapc <armel.pitelet@gmail.com>
2022-01-26 16:33:21 +0100

    qq notes wpf, angular, api

---
+++ WPF/Cours/V4/DemoBinding/DemoBinding/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using DemoBinding.ApiData;
+using DemoBinding.Dtos;
+using DemoBinding.Entities;
+using DemoBinding.Persistance;
+using DemonBinding.Models;
+using System.Net.Http;
+using System.Windows;
+using DemoBinding.UserControls;
+using DemoBinding.Utils;
+
+namespace DemoBinding
+{
+    /// <summary>
+    /// Interaction logic for App.xamlreé
+    /// </summary>
+    public partial class App : Application
+    {
+        private const string SERVER_URL = "https://localhost:7167"; 
+
+        public HttpClient HttpClient { get; } = new HttpClient();
+
+        public IDataManager<UserModel,UserDto> UserDataManager { get; } 
+
+        public IMapper Mapper { get; }
+
+        public INavigator Navigator { get; } = new Navigator();
+
+        public App()
+        {
+            var configuration = new MapperConfiguration(cfg => cfg.AddMaps(typeof(App)));
+            Mapper = new Mapper(configuration);
+            UserDataManager = new UserDataManager(HttpClient, Mapper, SERVER_URL);
+            Navigator.RegisterView(new UserDetailUC());
+        }
+    }
+}
