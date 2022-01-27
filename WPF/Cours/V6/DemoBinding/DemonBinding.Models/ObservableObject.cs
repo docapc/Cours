4d90a8c3916e4b5366b59237f4f81744582bdd11
docapc <armel.pitelet@gmail.com>
2022-01-27 16:32:34 +0100

    WPF : Cours ajouté jusqu'a v7. TODO trier et compiler les notes sur Angular, Wpf, Api

---
+++ WPF/Cours/V6/DemoBinding/DemonBinding.Models/ObservableObject.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DemonBinding.Models
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged; // 2vènement auquel on peut s'abonner.
+
+        protected virtual void OnNotifyPropertyChanged([CallerMemberName] string propertyname = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+            }
+        }
+    }
+}
---
+++ WPF/Cours/V7/DemoBinding/DemoBinding/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using DemoBinding.ApiData;
+using DemoBinding.Dtos;
+using DemoBinding.Entities;
+using DemoBinding.Persistance;
+using DemonBinding.Models;
+using Polly;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DemoBinding.UserControls;
+using DemoBinding.Utils;
+
+namespace DemoBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public INavigator Navigator { get; set; } = ((App) Application.Current).Navigator;
+   
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this; 
+            Navigator.NavigateTo(typeof(LoginPage));
+        }
+
+        
+
+        private void GoBackClick(object sender, RoutedEventArgs e)
+        {
+            if (Navigator.CanGoBack())
+            {
+                Navigator.Back();
+            }
+        }
+        
+    }
+}
